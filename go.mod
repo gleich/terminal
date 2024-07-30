a87ebbb64858225b9a3a544f13acf917483874e3
Matt Gleich <git@mattglei.ch>
2024-07-30 03:18:12 -0400

    feat: basic logging
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- go.mod
+++ go.mod
@@ -9,9 +9,10 @@ require (
 	golang.org/x/sys v0.22.0 // indirect
 )
 
+require github.com/gliderlabs/ssh v0.3.7
+
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
-	github.com/gliderlabs/ssh v0.3.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"io"
-	"log"
 
+	"github.com/gleich/lumber/v2"
 	"github.com/gliderlabs/ssh"
 )
 
 func main() {
 	ssh.Handle(func(s ssh.Session) {
-		io.WriteString(s, "Hello world\n")
+		lumber.Info("handling connection from", s.RemoteAddr().String())
+		_, err := io.WriteString(s, fmt.Sprintf("Hello %s\n", s.User()))
+		if err != nil {
+			lumber.Error(err, "failed to write string to user")
+		}
 	})
 
-	log.Fatal(ssh.ListenAndServe(":22", nil))
+	lumber.Info("starting server")
+	lumber.Fatal(ssh.ListenAndServe(":22", nil))
 }
