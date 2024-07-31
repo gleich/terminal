a529fb83ecb65279c5b11ed530027315f93baeb7
Matt Gleich <git@mattglei.ch>
2024-07-31 17:29:33 -0400

    feat: don't log http redirects
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- main.go
+++ main.go
@@ -29,7 +29,6 @@ func main() {
 func startHTTP() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "https://github.com/gleich/terminal", http.StatusTemporaryRedirect)
-		lumber.Success("handled http redirect to github repo")
 	})
 
 	lumber.Info("starting http server")
