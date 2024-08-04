c137006ab6cc466e925e6deefd65c433c4bebdc2
Matt Gleich <git@mattglei.ch>
2024-08-03 20:41:49 -0400

    feat: don't output activity index
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/cmds/workouts.go
+++ internal/cmds/workouts.go
@@ -26,7 +26,7 @@ func Workouts(s ssh.Session, out *termenv.Output, colors output.Colors) {
 	)
 	fmt.Fprintln(s)
 
-	for i, a := range response.Data {
+	for _, a := range response.Data {
 		t := table.New()
 		t.Border(lipgloss.NormalBorder())
 		t.BorderRow(true)
@@ -46,8 +46,7 @@ func Workouts(s ssh.Session, out *termenv.Output, colors output.Colors) {
 		)
 		fmt.Fprintf(
 			s,
-			"#%d: %s %s\n",
-			i+1,
+			"%s %s\n",
 			out.String(a.Name).Foreground(colors.Blue).Bold().Underline(),
 			out.String("["+util.RenderExactFromNow(a.StartDate)+"]").Foreground(colors.Grey),
 		)
