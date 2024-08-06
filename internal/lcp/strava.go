f18c12579586b51799c4cfe5044adbcf5eedd92d
Matt Gleich <git@mattglei.ch>
2024-08-05 21:04:04 -0400

    feat: update strava data model
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/lcp/strava.go
+++ internal/lcp/strava.go
@@ -11,30 +11,18 @@ import (
 )
 
 type StravaActivity struct {
-	Name      string    `json:"name"`
-	SportType string    `json:"sport_type"`
-	StartDate time.Time `json:"start_date"`
-	Timezone  string    `json:"timezone"`
-	Map       struct {
-		SummaryPolyline string  `json:"summary_polyline"`
-		MapBlurImage    *string `json:"map_blur_image"`
-	} `json:"map"`
-	Trainer            bool    `json:"trainer"`
-	Commute            bool    `json:"commute"`
-	Private            bool    `json:"private"`
-	AverageSpeed       float32 `json:"average_speed"`
-	MaxSpeed           float32 `json:"max_speed"`
-	AverageTemp        int32   `json:"average_temp,omitempty"`
-	AverageCadence     float32 `json:"average_cadence,omitempty"`
-	AverageWatts       float32 `json:"average_watts,omitempty"`
-	DeviceWatts        bool    `json:"device_watts,omitempty"`
-	AverageHeartrate   float32 `json:"average_heartrate,omitempty"`
-	TotalElevationGain float32 `json:"total_elevation_gain"`
-	MovingTime         uint32  `json:"moving_time"`
-	SufferScore        float32 `json:"suffer_score,omitempty"`
-	PrCount            uint32  `json:"pr_count"`
-	Distance           float32 `json:"distance"`
-	ID                 uint64  `json:"id"`
+	Name               string    `json:"name"`
+	SportType          string    `json:"sport_type"`
+	StartDate          time.Time `json:"start_date"`
+	Timezone           string    `json:"timezone"`
+	MapBlurImage       *string   `json:"map_blur_image"`
+	MapImageURL        *string   `json:"map_image_url"`
+	HasMap             bool      `json:"has_map"`
+	TotalElevationGain float32   `json:"total_elevation_gain"`
+	MovingTime         uint32    `json:"moving_time"`
+	Distance           float32   `json:"distance"`
+	ID                 uint64    `json:"id"`
+	AverageHeartrate   float32   `json:"average_heartrate"`
 }
 
 func FetchActivities() (Response[[]StravaActivity], error) {
