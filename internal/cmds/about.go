032e87e008ba03eca25713257261b2ac66bae844
Matt Gleich <git@mattglei.ch>
2025-05-21 22:59:19 -0400

    feat: new about msg
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/cmds/about.go
+++ internal/cmds/about.go
@@ -9,10 +9,14 @@ import (
 
 func about(s ssh.Session, styles output.Styles) {
 	linkStyle := styles.Blue.Underline(true)
+	fmt.Fprintln(s, "\nHey, I'm Matt Gleich")
 	fmt.Fprintln(
 		s,
 		styles.Renderer.NewStyle().Width(output.MAX_WIDTH).Render(fmt.Sprintf(
-			"\nI'm a third-year Computer Science student at the Rochester Institute of Technology (RIT). This website serves as a portfolio showcasing some of my projects, work experience, and personal interests. I am committed to applying my technical skills to develop practical solutions and consistently strive to expand my expertise. If you'd like to discuss a project or explore potential opportunities, please contact me at mail@mattglei.ch. Additional details about my professional work are in my résumé [%s].\n",
+			"I'm a rising fourth-year computer science student at the Rochester Institute of Technology [%s] and an incoming DevOps intern at KCF Technologies [%s]. This website serves as a portfolio showcasing some of my projects, work experience, and personal interests. I am committed to applying my technical skills to develop practical solutions and consistently strive to expand my expertise. If you'd like to discuss a project or explore potential opportunities, please contact me at %s. Additional details about my professional work are in my résumé [%s].\n",
+			linkStyle.Render("https://rit.edu"),
+			linkStyle.Render("https://kcftech.com"),
+			linkStyle.Render("mail@mattglei.ch"),
 			linkStyle.Render("https://mattglei.ch/resume.pdf"),
 		)),
 	)
