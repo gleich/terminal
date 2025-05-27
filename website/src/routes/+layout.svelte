8f372d1610b81b379aef579cb0fe77040d48ff07
Matt Gleich <git@mattglei.ch>
2025-05-26 23:27:48 -0400

    feat: remove link to favicon
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import '@gleich/ui/styles.css';
+	import { siteName } from '@gleich/ui';
+
+	siteName.set('terminal.mattglei.ch');
 
 	const { children }: { children: Snippet } = $props();
 </script>
