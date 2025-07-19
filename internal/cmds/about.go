13753027a4a155e2e2f564f714fca6c806ee67be
Matt Gleich <git@mattglei.ch>
2025-07-19 18:32:04 -0400

    feat: currently at kcf
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/cmds/about.go
+++ internal/cmds/about.go
@@ -13,7 +13,7 @@ func about(s ssh.Session, styles output.Styles) {
 	fmt.Fprintln(
 		s,
 		styles.Renderer.NewStyle().Width(output.MAX_WIDTH).Render(fmt.Sprintf(
-			"I'm a rising fourth-year computer science student at the Rochester Institute of Technology [%s] and an incoming DevOps intern at KCF Technologies [%s]. This website serves as a portfolio showcasing some of my projects, work experience, and personal interests. I am committed to applying my technical skills to develop practical solutions and consistently strive to expand my expertise. If you'd like to discuss a project or explore potential opportunities, please contact me at %s. Additional details about my professional work are in my résumé [%s].\n",
+			"I'm a rising fourth-year computer science student at the Rochester Institute of Technology [%s] and currently a DevOps intern at KCF Technologies [%s]. This website serves as a portfolio showcasing some of my projects, work experience, and personal interests. I am committed to applying my technical skills to develop practical solutions and consistently strive to expand my expertise. If you'd like to discuss a project or explore potential opportunities, please contact me at %s. Additional details about my professional work are in my résumé [%s].\n",
 			linkStyle.Render("https://rit.edu"),
 			linkStyle.Render("https://kcftech.com"),
 			linkStyle.Render("mail@mattglei.ch"),
