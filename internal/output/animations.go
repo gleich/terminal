79c45be3166d61619503d0f231855fb364eb4713
Matt Gleich <git@mattglei.ch>
2025-08-19 23:45:29 -0400

    feat: log error for fmt.Fprint
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/output/animations.go
+++ internal/output/animations.go
@@ -5,11 +5,15 @@ import (
 	"time"
 
 	"github.com/charmbracelet/ssh"
+	"go.mattglei.ch/timber"
 )
 
 func TypewriterAnimation(s ssh.Session, speed time.Duration, msg string) {
 	for _, c := range msg {
-		fmt.Fprint(s, string(c))
+		_, err := fmt.Fprint(s, string(c))
+		if err != nil {
+			timber.Error(err, "failed to output", c)
+		}
 		time.Sleep(speed)
 	}
 }
